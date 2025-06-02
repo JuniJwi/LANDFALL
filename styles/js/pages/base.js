c17572f01cfe6dd781d194b9db471a6806b99cb9
Juni <junikimdraws@gmail.com>
2025-06-02 10:09:50 -0500

    tab switching on bootstrap

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
 });
+
+// the tab id is sent after the #
+var hash = window.location.hash; 
+if (hash != "" && hash != null) {
+    const bsTab = new bootstrap.Tab(hash)
+    bsTab.show();
+}
