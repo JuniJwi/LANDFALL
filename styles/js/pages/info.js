157f4d4cce4b3d72cc886879adb91ee640d7c1b5
Juni <junikimdraws@gmail.com>
2025-06-02 12:41:58 -0500

    another attempt at tab switch

--- styles/js/pages/info.js
+++ styles/js/pages/info.js
@@ -2,10 +2,27 @@ document.addEventListener("DOMContentLoaded", async () => {
     
   /* Prompts
   ===================================================================== */
-  // the tab id is sent after the #
-    var hash = window.location.hash; 
-    if (hash != "" && hash != null) {
-        const bsTab = new bootstrap.Tab(hash)
-        bsTab.show();
+    // find if we have any tabs
+    if ($('.tab-content').length) {
+
+        //priority 1 : set active tab according to url
+        var url = window.location.hash;
+        console.log(url);
+        var activeTab = url.substring(url.indexOf("#") + 1);
+
+        if ( $('[data-bs-target="#'+activeTab+'"]').length) {
+            $('[data-bs-target="#'+activeTab+'"]').tab('show');
+        }
+
+        // priority 2 : keep activ tab according to local storage value
+        else {
+            $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+                localStorage.setItem('lastTab', $(this).attr('data-bs-target'));
+            });
+            var lastTab = localStorage.getItem('lastTab');
+            if (lastTab) {
+                $('[data-bs-target="' + lastTab + '"]').tab('show');
+            }
+        }
     }
 });
